650542b99cbfc6deb757454632f690612e898bb6
Connie Park <conniedc1206@gmail.com>
2022-05-26 08:27:39 -0700

    Completed 3 required deliverables

--- src/index.js
+++ src/index.js
@@ -1,60 +1,70 @@
-//fetch the data from the server
-//display images for each ramen using an img tag inside #ramen-men div
+//See all ramen images in the div with the id of ramen-menu. 
+//When the page loads, request the data from the server to get all the ramen objects. 
+//Then, display the image for each of the ramen using an img tag inside the #ramen-menu div.
+
 const ramen = () => {
     fetch('http://localhost:3000/ramens')
     .then(res => res.json())
     .then(data => {
         //console.log(data)
-        data.forEach(OneImage)
+        data.forEach(displayRamenImage)
     })
 }
 ramen()
 
-const OneImage = (oneRamen) => {
-    const ramenMenu = document.querySelector('#ramen-menu');
-    const pic = document.createElement('img');
-    pic.src = oneRamen.image;
-    ramenMenu.append(pic);
-    
-    //listen for click on image in #ramen-menu
-    //the ramen that was clicked on should appear on the ramen details and the comments and rating
+const displayRamenImage = (oneRamen) => {
+    const oneRamenImage = document.querySelector('#ramen-menu')
+    const pic = document.createElement('img')
+    pic.src = oneRamen.image
+    oneRamenImage.append(pic)
+    //console.log(pic)
+
+    //add 'click' event listener to pic
+    //when clicked, infro will be displayed in #ramen-detail div
     pic.addEventListener('click', () => {
         //console.log('clicked')
-        //const imageToUpdate = document.getElementsByClassName('detail-image')[0]
-        const imageToUpdate = document.querySelector('#ramen-detail img.detail-image')
-        imageToUpdate.src = oneRamen.image
+        const insertImage = document.querySelector('img.detail-image')
+        insertImage.src = oneRamen.image
 
-        const nameToUpdate = document.querySelector('#ramen-detail h2.name')
-        nameToUpdate.textContent = oneRamen.name
+        const insertName = document.querySelector('h2.name')
+        insertName.textContent = oneRamen.name
 
-        const restaurantToUpdate = document.querySelector('#ramen-detail h3.restaurant')
-        restaurantToUpdate.textContent = oneRamen.restaurant
+        const insertRestaurant = document.querySelector('h3.restaurant')
+        insertRestaurant.textContent = oneRamen.restaurant
 
-        const ratingToUpdate = document.querySelector('#rating-display')
-        ratingToUpdate.textContent = oneRamen.rating
+        const insertRating = document.querySelector('#rating-display')
+        insertRating.textContent = oneRamen.rating
 
-        const commentToUpdate = document.querySelector('#comment-display')
-        commentToUpdate.textContent = oneRamen.comment
+        const insertComment = document.querySelector('#comment-display')
+        insertComment.textContent = oneRamen.comment
     })
 }
 
 //Create a new ramen after submitting the new-ramen form. 
-//The new ramen should be added to the#ramen-menu div. The new ramen does not need to persist; in other words, if you refresh the page, it's okay that the new ramen is no longer on the page.
-document.querySelector('#new-ramen').addEventListener('submit',(e)=>{
+//create submit event listener
+//add preventDefault
+//create a new object with values from input 
+
+const form = document.querySelector('#new-ramen')
+form.addEventListener('submit', (e) => {
     e.preventDefault()
-    //console.log("submitted")
-    //grab target by name using dot notation (without hyphen)
-    //console.log(e.target.name.value)
-    //grab target by id using bracket notation(with hyphen)
-    //console.log(e.target['new-name'].value)
-    
+    //console.log('submitted')
+
     const newRamen = {
         name: e.target.name.value,
-        restaurant:e.target.restaurant.value,
+        restaurant: e.target.restaurant.value,
         image: e.target.image.value,
         rating: e.target.rating.value,
-        comment:e.target['new-comment'].value,
+        comment: e.target['new-comment'].value,
     }
-    console.log(newRamen)
-    OneImage(newRamen)
+    //console.log(newRamen)
+    
+    //The new ramen should be added to the#ramen-menu div. The new ramen does not need to persist; in other words, if you refresh the page, it's okay that the new ramen is no longer on the page.
+    displayRamenImage(newRamen)
+    
+    //then resets the form so you can input it again
+    form.reset()
 })
+
+
+
